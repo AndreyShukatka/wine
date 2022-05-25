8667c25846903f9a7b1498cc7695065d885fca15
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-05-25 13:03:04 +0300

    Структурировал код, переименовал переменные.

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+import collections
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-import datetime
 from pandas import read_excel
-import collections
 
-wines = read_excel('wine.xlsx', na_values=['N/A', 'NA'],
-                   keep_default_na=False).to_dict(orient='records')
-env = Environment(loader=FileSystemLoader('.'),
-                  autoescape=select_autoescape(['html', 'xml'])
-                  )
+Current_date = datetime.datetime.now().year
+foundation_date = 1920
 
 
 def all_alcohol():
@@ -20,32 +17,35 @@ def all_alcohol():
 
 def ending_word(last_digit):
     if last_digit == 0 or 5 <= last_digit <= 9:
-        renderer_page = template.render(year=year,
+        renderer_page = template.render(year=how_many_years,
                                         inscription='лет',
-                                        wines=wines)
+                                        wines=alcohols)
         return renderer_page
     elif last_digit == 1:
-        renderer_page = template.render(year=year,
+        renderer_page = template.render(year=how_many_years,
                                         inscription='год',
-                                        wines=wines)
+                                        wines=alcohols)
         return renderer_page
     else:
-        renderer_page = template.render(year=year,
+        renderer_page = template.render(year=how_many_years,
                                         inscription='года',
-                                        wines=wines)
+                                        wines=alcohols)
         return renderer_page
 
 
 if __name__ == '__main__':
+    wines = read_excel('wine.xlsx', na_values=['N/A', 'NA'],
+                       keep_default_na=False).to_dict(orient='records')
+    env = Environment(loader=FileSystemLoader('.'),
+                      autoescape=select_autoescape(['html', 'xml'])
+                      )
     template = env.get_template('template.html')
-    now = datetime.datetime.now().year
-    start_date = 1920
-    year = now - start_date
-    last_digit = year % 10
-    wines = all_alcohol()
+    how_many_years = Current_date - foundation_date
+    last_digit_year = how_many_years % 10
+    alcohols = all_alcohol()
 
     with open('index.html', 'w', encoding='utf8') as file:
-        file.write(ending_word(last_digit))
+        file.write(ending_word(last_digit_year))
 
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
